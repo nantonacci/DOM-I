6f902c9902c6644d9b5575360af9fa2e06c855dd
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-06-05 18:32:20 -0700

    Finished adding top and bottom content.

--- js/index.js
+++ js/index.js
@@ -51,3 +51,56 @@ roundImg.setAttribute('src', siteContent['cta']['img-src']);
 
 let middleImg = document.querySelector('#middle-img');
 middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+// nav
+let navLinks = document.querySelectorAll('nav a');
+
+navLinks[0].textContent = 'Services';
+navLinks[1].textContent = 'Product';
+navLinks[2].textContent = 'Vision';
+navLinks[3].textContent = 'Features';
+navLinks[4].textContent = 'About';
+navLinks[5].textContent = 'Contact';
+
+// cta
+let ctaH1 = document.querySelector('h1');
+
+// !!!!!!!!!!!!!--------NEEDS BREAKS----------!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ctaH1.textContent = 'Dom is Awesome';
+//DOM<br> Is<br> Awesome
+
+let ctaButton = document.querySelector('button');
+ctaButton.textContent = 'Get Started';
+
+// top-content
+let topH4s = document.querySelectorAll('.top-content h4');
+topH4s[0].textContent = 'Features';
+topH4s[1].textContent = 'About';
+
+let topPs = document.querySelectorAll('.top-content p');
+topPs[0].textContent =
+  'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+topPs[1].textContent =
+  'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+//bottom-content
+let bottomH4s = document.querySelectorAll('.bottom-content h4');
+bottomH4s[0].textContent = 'Services';
+bottomH4s[1].textContent = 'Product';
+bottomH4s[2].textContent = 'Vision';
+
+let bottomPs = document.querySelectorAll('.bottom-content p');
+bottomPs[0].textContent =
+  'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+bottomPs[1].textContent =
+  'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+bottomPs[2].textContent =
+  'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+//.contact h4 = "Contact"
+//.contact p = "123 Way 456 Street" </br> "Somewhere, USA"
+//.contact p = "1 (888) 888-8888"
+//.contact p = sales@greatidea.io
+
+//footer p = "Copyright Great Idea! 2018"
